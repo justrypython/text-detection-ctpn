bddf44b5c4e592f8c9948588b0b6abc9c48d7bfb
kerzhao <kerzhao@163.com>
2017-09-26 14:14:33 +0800

    update

--- lib/roi_data_layer/__init__.py
+++ lib/roi_data_layer/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # --------------------------------------------------------
 # Fast R-CNN
 # Copyright (c) 2015 Microsoft
@@ -6,12 +5,3 @@
 # Written by Ross Girshick
 # --------------------------------------------------------
 import roidb
-=======
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
-from . import roidb
->>>>>>> bb4142e43a4ee8c3f539b7b17ed9649d3d973dd8
